fb6b48c2c0e4eaa178dd9b39baf04010059e0d5e
jazcash <jaspercashmore@gmail.com>
2016-11-28 12:21:46 +0000

    Added animation example

---
+++ public/scripts/animation/fadeSlide.js
@@ -0,0 +1,15 @@
+// $("[class*=fadeslide]")
+
+function checkFadeSlide(){
+	let thisScroll = $(window).scrollTop() + $(window).outerHeight();
+	$("[class*=fadeslide]").each(function(e, el){
+		let elPos = $(this).offset().top;
+		if (thisScroll > elPos){
+			$(this).addClass("fadeslide-active");
+		}
+	});
+}
+
+checkFadeSlide();
+
+$(window).scroll(checkFadeSlide);
